1cc01ce5203d6a9ea5b1b604e4e7cefbd1300388
antoniu.pitic <antoniu.pitic@gmail.com>
2023-01-16 19:54:37 +0200

    produs

--- papagal/papagal.cpp
+++ papagal/papagal.cpp
@@ -7,7 +7,13 @@ int Suma(int a, int b) {
     return a + b;
 }
 
+int Produs(int a, int b) {
+    return a * b;
+}
+
+
 int main()
 {
     assert(Suma(3, 5) == 8);
+    assert(Produs(3, 5) == 15);
 }
